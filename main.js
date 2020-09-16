f0c862e25465638a98cdd9208e01ac0b8e0a28b8
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-09-16 14:47:07 -0400

    finished

--- main.js
+++ main.js
@@ -3,78 +3,83 @@
 //the loop should not return a data structure
 // Use one or more of the object methods to solve
 const obj = {
-    user1: {
-      name: 'Jon',
-      age: 23,
-    },
-    user2: {
-      name: 'Jen',
-      age: 25,
-    },
-    user3: {
-      name: 'Jan',
-      age: 21,
-    },
-  };
-  
-  let arr = Object.entries(obj)
-  let newArr = arr.forEach(x => console.log(`${x[0]} is called ${x[1].name} and is ${x[1].age} years old` )) 
-  console.log(newArr)
-  
-  
-  
-  
-  
-  // Use Object Methods
-  //Create an albphabeticl list of every folder in the documents object (the keys are the folders)
-  //Create a list of every file in the documents object. one list.
-  const documents = {
-    taxes: ['2010.xlx', '2020.xlx'],
-    records: ['rec1.docx', 'rec2.docx', 'rec3.docx'],
-    miscellaneous: ['pic.jpg', 'passwords.pdf', 'docs.txt'],
-    Clubhouse: ['paty.jpg', 'christmasRules.pdf', 'pool.txt'],
-  };
-  
-  console.log(Object.keys(documents))
-  
-  //Are the two books alike? Create a function and Use Object methods to find out. Change object values to test for non-alike objects
-  const book1 = {
-    title: 'My Book',
-    author: 'John Doe',
-    year: '2019',
-  };
-  const book2 = {
-    title: 'My Book',
-    author: 'John Doe',
-    year: '2019',
-  };
-  
-  
-  let comparison = (book1, book2) => {
-  let values1 = Object.values(book1);
-  let values2 = Object.values(book2);
-  if(values1.length !== values2.length){
-  return false
-  }
-  for(let i = 0; i < values1.length;i++){
-  if(values1[i] !== values2[i]){
-  return false
-  }
-  
-  }
-  return true
-  }
-  
-  
-  
-  
-  
-  comparison(book1, book2)
-  
-  
-  
-  
-  
-  
-  
-  
+  user1: {
+    name: 'Jon',
+    age: 23,
+  },
+  user2: {
+    name: 'Jen',
+    age: 25,
+  },
+  user3: {
+    name: 'Jan',
+    age: 21,
+  },
+};
+
+let arr = Object.entries(obj)
+let newArr = arr.forEach(x => console.log(`${x[0]} is called ${x[1].name} and is ${x[1].age} years old` )) 
+console.log(newArr)
+
+
+
+
+
+// Use Object Methods
+//Create an albphabeticl list of every folder in the documents object (the keys are the folders)
+//Create a list of every file in the documents object. one list.
+const documents = {
+  taxes: ['2010.xlx', '2020.xlx'],
+  records: ['rec1.docx', 'rec2.docx', 'rec3.docx'],
+  miscellaneous: ['pic.jpg', 'passwords.pdf', 'docs.txt'],
+  Clubhouse: ['paty.jpg', 'christmasRules.pdf', 'pool.txt'],
+};
+
+console.log(Object.keys(documents))
+
+let together = Object.keys(documents).reduce(function(arr, key) {
+  return arr.concat(documents[key]);
+}, []);
+
+console.log(together);
+
+//Are the two books alike? Create a function and Use Object methods to find out. Change object values to test for non-alike objects
+const book1 = {
+  title: 'My Book',
+  author: 'John Doe',
+  year: '2019',
+};
+const book2 = {
+  title: 'My Book',
+  author: 'John Doe',
+  year: '2019',
+};
+
+
+let comparison = (book1, book2) => {
+let values1 = Object.values(book1);
+let values2 = Object.values(book2);
+if(values1.length !== values2.length){
+return console.log(false)
+}
+for(let i = 0; i < values1.length;i++){
+if(values1[i] !== values2[i]){
+return console.log(false)
+}
+
+}
+return console.log(true)
+}
+
+
+
+
+
+comparison(book1, book2)
+
+
+
+
+
+
+
